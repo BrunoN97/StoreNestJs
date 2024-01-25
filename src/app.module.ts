9a95832060c744c4866ad524d34b68581f5c82a3
Bruno Nobre <nobre.piaget@gmail.com>
2024-01-25 16:03:32 -0300

    add config db e .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/user.module';
 import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './users/config/db.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [
+    UserModule,
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
